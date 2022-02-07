42b03ad9244fb465106ab99fbe8fdfe93aec5921
Sam A <samuelanoff1@gmail.com>
2022-02-07 11:16:43 -0500

    Fixed casing for orderId

--- quadpay/src/main/java/com/quadpay/quadpay/QuadPayVirtualCheckoutDelegate.java
+++ quadpay/src/main/java/com/quadpay/quadpay/QuadPayVirtualCheckoutDelegate.java
@@ -1,7 +1,7 @@
 package com.quadpay.quadpay;
 
 public interface QuadPayVirtualCheckoutDelegate {
-    void checkoutSuccessful(QuadPayCard card, QuadPayCardholder cardholder, QuadPayCustomer customer, string orderId);
+    void checkoutSuccessful(QuadPayCard card, QuadPayCardholder cardholder, QuadPayCustomer customer, String orderId);
     void checkoutCancelled(String reason);
     void checkoutError(String error);
 }
